d1176811ef4935031188e3f02ceff817575baf6a
Claudio <claudio.marino@outlook.com>
2025-07-31 19:24:01 +0200

    ADDED THE CONTEX OF CONVEX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-createRoot(document.getElementById('root')).render(
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ConvexProvider client={convex}>
+      <App />
+    </ConvexProvider>
   </StrictMode>,
-)
+);
