a2784af8c21ce8cbe6a84e22d912d66dd7158e5f
Claudio <claudio.marino@outlook.com>
2025-08-04 23:45:46 +0200

    CHANGED A LITTLE BIT THE STYLES OF THE TAB

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -3,19 +3,19 @@ import { CardOutline, StatsChartOutline } from "react-ionicons";
 
 export default function TabBar({ showPedidosPage, setShowPedidosPage }) {
   return (
-    <div className="bg-light fixed bottom-0 z-10 flex w-full items-baseline justify-center gap-36 px-16 py-5">
+    <div className="bg-light fixed bottom-0 z-10 flex w-full items-baseline justify-center gap-36 px-16 pb-7 pt-3.5">
       <div
         onClick={() => setShowPedidosPage(true)}
         className={`${showPedidosPage ? "text-secondary" : "text"} flex flex-col items-center justify-center gap-1`}
       >
-        <CardOutline width="20px" />
+        <CardOutline width="25px" />
         <p className="text-xs">Pedidos</p>
       </div>
       <div
         onClick={() => setShowPedidosPage(false)}
         className={`${!showPedidosPage ? "text-secondary" : "text"} flex flex-col items-center justify-center gap-1`}
       >
-        <StatsChartOutline className="" width="20px" />
+        <StatsChartOutline className="" width="25px" />
         <p className="text-xs">Stats</p>
       </div>
     </div>
