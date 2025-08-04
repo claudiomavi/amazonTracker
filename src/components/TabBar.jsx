0dcb5784311e8be3558d847a2943a4be3b389bcc
Claudio <claudio.marino@outlook.com>
2025-08-04 17:46:20 +0200

    MADE THE TABBAR SMALLER

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -5,12 +5,12 @@ export default function TabBar() {
   return (
     <div className="bg-light fixed bottom-0 z-10 flex w-full items-baseline justify-center gap-36 px-16 py-5">
       <div className="text-secondary flex flex-col items-center justify-center gap-1">
-        <CardOutline width="30px" />
-        <p className="text-sm">Pedidos</p>
+        <CardOutline width="20px" />
+        <p className="text-xs">Pedidos</p>
       </div>
       <div className="text flex flex-col items-center justify-center gap-1">
-        <StatsChartOutline className="" width="30px" />
-        <p className="text-sm">Stats</p>
+        <StatsChartOutline className="" width="20px" />
+        <p className="text-xs">Stats</p>
       </div>
     </div>
   );
